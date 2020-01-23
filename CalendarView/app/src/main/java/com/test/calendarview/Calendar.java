01299b0ff5b39e06cb3fb1050ab3848522f8eec0
Maël Bervet <mael.bervet@gmail.com>
2020-01-23 19:34:46 +0100

    Attributs de la vue géré en xml plus en java

--- CalendarView/app/src/main/java/com/test/calendarview/Calendar.java
+++ CalendarView/app/src/main/java/com/test/calendarview/Calendar.java
@@ -1,6 +1,8 @@
 package com.test.calendarview;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
@@ -9,56 +11,69 @@ import android.widget.CalendarView;
 import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class Calendar extends CalendarView {
 
     private final int ACTION_SCROLL_UP = 1;
     private final int ACTION_SCROLL_DOWN = 2;
     private final int ACTION_LONG_CLICK = 3;
-    private final int LongClickTimeUP = 300;
 
     private int TouchSlop;
+    private int LongClickSlop;
     private int Action = 0;
 
     public Calendar(@NonNull Context context) {
         super(context);
-        setFirstDayOfWeek(2);
-        setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        setLongClickable(true);
+        InitializeSlops(context);
+    }
+
+    public Calendar(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        InitializeSlops(context);
+    }
+
+    public Calendar(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        InitializeSlops(context);
+    }
+
+    private void InitializeSlops(Context context){
         ViewConfiguration vc = ViewConfiguration.get(context);
         TouchSlop = vc.getScaledTouchSlop();
+        LongClickSlop = 1000;
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        if(event.getActionMasked() != MotionEvent.ACTION_MOVE && event.getHistorySize() == 0){
-            if (event.getEventTime() - event.getDownTime() > LongClickTimeUP){
+        boolean result = false;
+
+        if(event.getHistorySize() == 0 || event.getActionMasked() != MotionEvent.ACTION_MOVE){
+            if (event.getEventTime() - event.getDownTime() > LongClickSlop){
                 Action = ACTION_LONG_CLICK;
 
-                return true;
+                result = true;
             }
+            else {
+                result = false;
+            }
+        }
+        else {
+            float xDiff = Math.abs(event.getHistoricalX(0) - event.getX());
+            float yDiff = event.getHistoricalY(0) - event.getY();
 
-            return false;
+            if (Math.abs(yDiff) > xDiff && Math.abs(yDiff) > TouchSlop) {
+                if (yDiff < 0) {
+                    Action = ACTION_SCROLL_DOWN;
+                } else if (yDiff > 0) {
+                    Action = ACTION_SCROLL_UP;
+                }
+
+                result = true;
+            }
         }
 
-        System.out.println(event.getHistorySize());
-        System.out.println(event.getDownTime() - event.getEventTime());
-
-        float xDiff = Math.abs(event.getHistoricalX(0) - event.getX());
-        float yDiff = event.getHistoricalY(0) - event.getY();
-        System.out.println(xDiff + " " + yDiff);
-
-        if (Math.abs(yDiff) > xDiff && Math.abs(yDiff) > TouchSlop) {
-            if (yDiff < 0) {
-                Action = ACTION_SCROLL_DOWN;
-            } else if (yDiff > 0) {
-                Action = ACTION_SCROLL_UP;
-            }
-
-            return true;
-        }
-
-        return false;
+        return result;
     }
 
     @Override
@@ -80,7 +95,7 @@ public class Calendar extends CalendarView {
             setDate(getDate() - TimeUnit.DAYS.toMillis(365));
         }
         else if (Action == ACTION_LONG_CLICK){
-            System.out.println("LongPress");
+            Log.d("Calendar","LongPress");
         }
 
         return super.performClick();
--- CalendarView/app/src/main/java/com/test/calendarview/MainActivity.java
+++ CalendarView/app/src/main/java/com/test/calendarview/MainActivity.java
@@ -1,10 +1,8 @@
 package com.test.calendarview;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GestureDetectorCompat;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Calendar = new Calendar(this);
-        this.addContentView(Calendar, Calendar.getLayoutParams());
+        Calendar = findViewById(R.id.Calendar);
     }
 }
